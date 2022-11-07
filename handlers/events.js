d85e133cf01d3aefd7f4b383c8570bffcf7d1b86
INSANEMODE <scottshome02@gmail.com>
2022-11-07 00:03:43 -0600

    fix ready.js, so it runs properly

--- handlers/events.js
+++ handlers/events.js
@@ -11,7 +11,7 @@ module.exports = (client) => {
 			let pull = require(`../events/${dir}/${file}`);
 			if (pull.name) {
 				client.events.set(pull.name, pull);
-				console.log(`[HANDLER - EVENTS] Loaded a file: ${pull.name}`.brightGreen)
+				console.log(`[HANDLER - EVENTS] Loaded a file: ${pull.name}`.brightGreen);
 			} else {
 				console.log(`[HANDLER - EVENTS] Couldn't load the file ${file}. missing name or aliases.`.red)
 				continue;
