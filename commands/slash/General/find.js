d7289c9b2dd2bbe5f2066293a79d80be1f99ef6e
INSANEMODE <scottshome02@gmail.com>
2023-04-26 03:16:43 -0500

    - add joi for input validation - validate login user input - validate find command input

--- commands/slash/General/find.js
+++ commands/slash/General/find.js
@@ -1,7 +1,16 @@
 const { EmbedBuilder } = require('discord.js');
 const fetch = require('node-fetch');
 const stringtable = require('string-table');
+const joi = require('joi');
+const schema = joi.object({
 
+    user: joi.string()
+        .pattern(new RegExp('^[A-Za-z0-9!@#$%^&*_+=,.<>\/?-]{3,32}$'))
+        .min(3)
+        .max(32)
+        .required(),
+
+})
 module.exports = {
     name: "find", // Name of command
     description: "searches for a user", // Command description
@@ -20,19 +29,26 @@ module.exports = {
         await interaction.deferReply({ ephemeral: true });
 
         var user = interaction.options.get('user')?.value;
-        console.warn('find: user: ' + user);
+        const result = schema.validate({
+                    user:user
+                })
+        console.warn('find: user: ' + result.value.user);
+        if(result.error != undefined)
+        {
+            return interaction.editReply({ ephemeral: true, content: result.error.message, failIfNotExists: false});
+        }
         let query;
-        if (/^[0-9A-F]+$/.test(user)) query = "xuid=" + user;
-        else query = "name=" + user;
+        if (/^[0-9A-F]+$/.test(result.value.user)) query = "xuid=" + result.value.user;
+        else query = "name=" + result.value.user;
         console.warn('find: query: ' + query);
         let response = await fetch(config.Client.webfronturl + '/api/client/find??count=50&' + query)
             .catch(() => { console.log('\x1b[31mWarning: ' + config.Client.webfronturl + ' not reachable\x1b[0m') });
-        console.warn('find: response: ' + response);
+        console.warn('find: response: ' + response.status);
         if (!response) return interaction.editReply({ ephemeral: true, content: "Cannot establish connection to <" + config.Client.webfronturl + ">" });
         if (response.status === 400) return interaction.editReply({ephemeral: true, content: "The length of 'Name' must be at least 3 characters"});
 
         let data = await response.json();
-        if (data.totalFoundClients === 0) return interaction.editReply({ ephemeral: true, content: "No players found with provided " + (/^[0-9A-F]+$/.test(user) ? 'xuid' : 'name') });
+        if (data.totalFoundClients === 0) return interaction.editReply({ ephemeral: true, content: "No players found with provided " + (/^[0-9A-F]+$/.test(result.value.user) ? 'xuid' : 'name') });
 
         let arr = data.clients.map(obj => { return { Name: obj.name.replace(/\^[0-9:;c]/g, ''), ClientId: obj.clientId, XUID: obj.xuid } });
         let tad = stringtable.create(arr);
--- commands/slash/Modals/login.js
+++ commands/slash/Modals/login.js
@@ -1,7 +1,25 @@
 const { EmbedBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require("discord.js");
 const fetch = require('node-fetch');
+const joi = require('joi');
 const dbutils = require('../../../include/dbutils');
+const schema = joi.object({
+    username: joi.string()
+        .alphanum()
+        .min(1)
+        .max(16)
+        .required(),
+    password: joi.string()
+        .pattern(new RegExp('^[A-Za-z0-9!@#$%^&*_+=,.<>\/?-]{4,32}$'))
+        .min(4)
+        .max(32)
+        .required()
+        .messages({
+            "string.pattern.base": `Password should be between 4 to 32 characters and contain letters, numbers or the following special characters !@#$%^&*_+=,.<>/?-`,
+            "string.empty": `Password cannot be empty`,
+            "any.required": `Password is required`,
+          }),
 
+})
 module.exports = {
     name: "login",
     description: "!rt in game, or request token on webfront to get login info",
@@ -30,7 +48,7 @@ module.exports = {
             .setStyle(TextInputStyle.Short)
             .setPlaceholder('example: aB12')
             .setMaxLength(32)
-            .setMinLength(1)
+            .setMinLength(4)
             .setRequired(true);
         const ActionRow = new ActionRowBuilder().addComponents(clientid);
         const ActionRow2 = new ActionRowBuilder().addComponents(password);
@@ -46,16 +64,23 @@ module.exports = {
 
                 const cid = interaction.fields.getTextInputValue('clientid');
                 const pass = interaction.fields.getTextInputValue('pass');
-                if (isNaN(parseInt(cid))) return interaction.reply({ ephemeral: true, content: "Incorrect login details provided. Client id must be a number. Login create cancelled", failIfNotExists: false});
-
-                const response = await fetch(config.Client.webfronturl + '/api/client/' + parseInt(cid) + '/login', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: `{"password":"` + pass + `"}` })
+                const result = schema.validate({
+                    username:cid,
+                    password:pass
+                })
+                if(result.error != undefined)
+                {
+                    return interaction.reply({ ephemeral: true, content: result.error.message, failIfNotExists: false});
+                }
+                if (isNaN(parseInt(result.value.username))) return interaction.reply({ ephemeral: true, content: "Incorrect login details provided. Client id must be a number. Login create cancelled", failIfNotExists: false});
+                const response = await fetch(config.Client.webfronturl + '/api/client/' + parseInt(result.value.username) + '/login', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: `{"password":"` + result.value.password + `"}` })
                     .catch(() => { console.log('\x1b[31mWarning: ' + config.Client.webfronturl + ' not reachable\x1b[0m') });
 
                 if (!response) return interaction.reply({ ephemeral: true, content: "Cannot establish connection to <" + config.Client.webfronturl + ">" , failIfNotExists: false});
                 if (!(response.status == 200)) return interaction.reply({ ephemeral: true, content: "Incorrect login details provided. Login create cancelled", failIfNotExists: false });
 
                 var value = response.headers.get('set-cookie').split(';').findIndex(element => element.includes(".AspNetCore.Cookies"));
-                dbutils.insertData(interaction.member.id, parseInt(cid), response.headers.get('set-cookie').split(';')[value]);
+                dbutils.insertData(interaction.member.id, parseInt(result.value.username), response.headers.get('set-cookie').split(';')[value]);
 
                 interaction.reply({ ephemeral: true, content: "Success! your login is successfully stored.\nNote: We do not know or store your id and password", failIfNotExists: false});
 
